073a1e2bcc4c13d24ced316cb194a177db2fe3d4
Stefanie Hansen <stefuhnee@gmail.com>
2016-06-15 12:34:51 -0700

    mvp

--- stefanie-hansen/app/js/controllers/game-controller.js
+++ stefanie-hansen/app/js/controllers/game-controller.js
@@ -34,27 +34,30 @@ function GameController() {
   this.monsterRoom.hasWeaponActions = {
     'yolo': this.yolo,
     'reason': this.reason,
-    'hadouken': this.hadouken,
     'leave': this.exit
   };
 }
 
 GameController.prototype.action = function() {
   let input = this.input.toLowerCase();
-  console.log(input);
-  if (!input || this.actions.indexOf(input) === -1) {
-    this.tryAgain();
+  if (this.gameLog.length > 10) {
+    this.gameLog = [];
   }
-  else if (this.location === 'monsterRoom') {
+  if (input === '?') {
+    this.seeActions();
+  } else if (!input || this.actions.indexOf(input) === -1) {
+    this.tryAgain();
+  } else if (this.location === 'monsterRoom') {
     if (this.hasWeapon === false) {
       this.monsterRoom.noWeaponActions[input].call(this);
+    } else {
+      this.monsterRoom.hasWeaponActions[input].call(this);
     }
-    this.monsterRoom.hasWeaponActions[input].call(this);
-  }
-  else if (this.hasWeapon === false) {
+  } else if (this.hasWeapon === false) {
     this.weaponRoom.noWeaponActions[input].call(this);
+  } else {
+    this.weaponRoom.hasWeaponActions[input].call(this);
   }
-  this.weaponRoom.hasWeaponActions[input].call(this);
 };
 
 GameController.prototype.tryAgain = function() {
@@ -64,10 +67,9 @@ GameController.prototype.tryAgain = function() {
 GameController.prototype.seeActions = function() {
   let location = (this.location === 'monsterRoom') ? this.monsterRoom : this.weaponRoom;
   let actions = (this.hasWeapon === true) ? location.hasWeaponActions : location.noWeaponActions;
-  for (let i = 0; i < actions.length; i++) {
-    this.gameLog.push(actions[i]);
+  for (let key in actions) {
+    this.gameLog.push(key);
   }
-  return this.gameLog;
 };
 
 GameController.prototype.hyperventilate = function() {
@@ -79,7 +81,7 @@ GameController.prototype.hyperventilate = function() {
 
 GameController.prototype.pickUpWeapon = function() {
   if (this.location === 'weaponRoom' && this.hasWeapon === false) {
-    this.hasWeapon === true;
+    this.hasWeapon = true;
     return this.gameLog.push('You are now in possession of a feral weasel. Congratulations?');
   }
   this.tryAgain();
@@ -87,7 +89,7 @@ GameController.prototype.pickUpWeapon = function() {
 
 GameController.prototype.philosophize = function() {
   if (this.location === 'weaponRoom' && this.hasWeapon === true) {
-    return this.gameLog.push('You consider the moral and ethical implications of killing another lifeform. But you decide that you\'d be doing the monster a favor, as he knows no life beyond those four digital walls.');
+    return this.gameLog.push('You consider the moral and ethical implications of killing a sentient being. But you decide that you\'d be doing the monster a favor, as he knows no life beyond those four digital walls.');
   }
   this.tryAgain();
 };
@@ -115,9 +117,23 @@ GameController.prototype.callMom = function() {
   this.tryAgain();
 };
 
-GameController.prototype.callMom = function() {
+GameController.prototype.taunt = function() {
   if (this.location === 'monsterRoom' && this.hasWeapon === false) {
     return this.gameLog.push('You taunt the monster and he is taken aback with your stupidity. He remains lost in thought.');
+  }
+  this.tryAgain();
+};
+
+GameController.prototype.yolo = function() {
+  if (this.location === 'monsterRoom' && this.hasWeapon === true) {
+    return this.gameLog.push('Your feral weasel goes for monster\'s face! Thou hath slain the Jabberwock! Oh, frabjous day!');
+  }
+  this.tryAgain();
+};
+
+GameController.prototype.reason = function() {
+  if (this.location === 'monsterRoom' && this.hasWeapon === true) {
+    return this.gameLog.push('You try to reason with the monster and tell him that you don\'t want to have to kill him, but he does not speak English. Sorry. You die.');
   }
   this.tryAgain();
 };
@@ -125,4 +141,8 @@ GameController.prototype.callMom = function() {
 GameController.prototype.exit = function() {
   this.location === 'monsterRoom' ? this.location = 'weaponRoom' : this.location = 'monsterRoom';
   this.gameLog.push('You walk into the adjacent room.');
+};
+
+GameController.prototype.gameStart = function() {
+  this.gameLog.push('You wake up and find yourself in a windowless room with a fearsome Jabberwocky. He is eyeing you with a mixture of curiosity and hunger. There is a door to your left. What shall you do? (Type ? at any time to see your options)');
 };
