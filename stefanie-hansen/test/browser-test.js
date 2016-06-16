34caf97c8ac2fbb9b11b349718cf130e5033d744
Stefanie Hansen <stefuhnee@gmail.com>
2016-06-16 10:03:59 -0700

    the most tests

--- stefanie-hansen/test/browser-test.js
+++ stefanie-hansen/test/browser-test.js
@@ -62,5 +62,75 @@ describe('Controller tests', () => {
       gameCtrl.action();
       expect(gameCtrl.gameLog[0]).toBe('You try to express your situation to your mother, but she thinks that you\'ve had too much to drink. Your dependence on your mother annoys the monster and he impales you with a talon.');
     });
+
+    it('should give an appropriate response to the input "hyperventilate"', () => {
+      gameCtrl.location = 'weaponRoom';
+      gameCtrl.input = 'hyperventilate';
+      gameCtrl.action();
+      expect(gameCtrl.gameLog[0]).toBe('You get lightheaded and make poor decisions');
+    });
+
+    it('should give an appropriate response to the input "pick up weapon"', () => {
+      gameCtrl.location = 'weaponRoom';
+      gameCtrl.input = 'pick up weapon';
+      gameCtrl.action();
+      expect(gameCtrl.gameLog[0]).toBe('You are now in possession of a feral weasel. Congratulations?');
+      expect(gameCtrl.hasWeapon).toBe(true);
+    });
+
+    it('should give an appropriate response to the input "philosophize"', () => {
+      gameCtrl.location = 'weaponRoom';
+      gameCtrl.hasWeapon = true;
+      gameCtrl.input = 'philosophize';
+      gameCtrl.action();
+      expect(gameCtrl.gameLog[0]).toBe('You consider the moral and ethical implications of killing a sentient being. But you decide that you\'d be doing the monster a favor, as he knows no life beyond those four digital walls.');
+    });
+
+    it('should give an appropriate response to the input "poke"', () => {
+      gameCtrl.input = 'poke';
+      gameCtrl.action();
+      expect(gameCtrl.gameLog[0]).toBe('You edge toward the monster and poke him, then giggle again run away. The monster is furious and decapitates you in one fell chomp.');
+      expect(gameCtrl.alive).toBe(false);
+    });
+
+    it('should give an appropriate response to the input "whimper"', () => {
+      gameCtrl.input = 'whimper';
+      gameCtrl.action();
+      expect(gameCtrl.gameLog[0]).toBe('You whimper softly in the corner, hoping that the monster will let you live out your days... until you die of starvation. You should probably try to kill the monster so at least you have something to eat.');
+    });
+
+    it('should give an appropriate response to the input "call mom"', () => {
+      gameCtrl.input = 'call mom';
+      gameCtrl.action();
+      expect(gameCtrl.gameLog[0]).toBe('You try to express your situation to your mother, but she thinks that you\'ve had too much to drink. Your dependence on your mother annoys the monster and he impales you with a talon.');
+      expect(gameCtrl.alive).toBe(false);
+    });
+
+    it('should give an appropriate response to the input "taunt"', () => {
+      gameCtrl.input = 'taunt';
+      gameCtrl.action();
+      expect(gameCtrl.gameLog[0]).toBe('You taunt the monster and he is taken aback with your stupidity. He remains lost in thought.');
+    });
+
+    it('should give an appropriate response to the input "yolo"', () => {
+      gameCtrl.hasWeapon = true;
+      gameCtrl.input = 'yolo';
+      gameCtrl.action();
+      expect(gameCtrl.gameLog[0]).toBe('Your feral weasel goes for monster\'s face! Thou hath slain the Jabberwock! Oh, frabjous day!');
+    });
+
+    it('should give an appropriate response to the input "reason"', () => {
+      gameCtrl.hasWeapon = true;
+      gameCtrl.input = 'reason';
+      gameCtrl.action();
+      expect(gameCtrl.gameLog[0]).toBe('You try to reason with the monster and tell him that you don\'t want to have to kill him, but he does not speak English. Sorry. You die.');
+    });
+
+    it('should give an appropriate response to the input "leave"', () => {
+      gameCtrl.input = 'leave';
+      gameCtrl.action();
+      expect(gameCtrl.gameLog[0]).toBe('You walk into the adjacent room.');
+      expect(gameCtrl.location).toBe('weaponRoom');
+    });
   });
 });
